8718595d67611183f00958d0433462cf599cf1c7
Admin <shapovals1991@gmail.com>
2023-05-30 14:49:54 +0300

    refactoring code

--- person-service/src/main/java/com/personservice/personservice/domain/Person.java
+++ person-service/src/main/java/com/personservice/personservice/domain/Person.java
@@ -14,7 +14,7 @@ import lombok.*;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column(name = "first_name")
     private String firstName;
